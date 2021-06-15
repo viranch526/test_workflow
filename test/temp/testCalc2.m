96de210ffc9fdbb7ad180162ffd43bfd1560c1a0
viranch526 <85679468+viranch526@users.noreply.github.com>
2021-06-15 11:37:23 +0530

    Update testCalc2.m

--- test/temp/testCalc2.m
+++ test/temp/testCalc2.m
@@ -1,4 +1,4 @@
-classdef testCalc < matlab.unittest.TestCase
+classdef testCalc2 < matlab.unittest.TestCase
     %TESTCALC Summary of this class goes here
     %   Detailed explanation goes here
     
