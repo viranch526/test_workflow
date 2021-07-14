d46767ca15f92641317eca3694ee68923e955247
viranch526 <85679468+viranch526@users.noreply.github.com>
2021-07-14 13:09:11 +0530

    Update testCalc1.m

--- test/testCalc1.m
+++ test/testCalc1.m
@@ -1,4 +1,4 @@
-classdef testCalc < matlab.unittest.TestCase
+classdef testCalc1 < matlab.unittest.TestCase
     %TESTCALC Summary of this class goes here
     %   Detailed explanation goes here
     
